35a910d012210de3f0ea8af08d82e12779fa7d09
kornel <kornel@ADMINRG-Q1FUQ95>
2020-12-09 16:23:23 +0530

    commit by kornel

---
+++ databases/newwayshopping.sql
@@ -0,0 +1,125 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 09, 2020 at 11:51 AM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `newwayshopping`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(250) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `product_type` varchar(100) NOT NULL,
+  `product_cost` varchar(100) NOT NULL,
+  `product_image` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `serial_number` int(250) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `confirm_password` varchar(40) NOT NULL,
+  `phone_number` int(20) NOT NULL,
+  `alternative_phone_number` int(20) DEFAULT NULL,
+  `date_of_birth` date NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `address_1` varchar(500) NOT NULL,
+  `address_2` varchar(500) DEFAULT NULL,
+  `country` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `postal_code` int(10) NOT NULL,
+  `image` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `saler`
+--
+
+CREATE TABLE `saler` (
+  `saler_id` int(250) NOT NULL,
+  `saler_first_name` varchar(100) NOT NULL,
+  `saler_last_name` varchar(100) NOT NULL,
+  `saler_email` varchar(100) NOT NULL,
+  `saler_gender` varchar(50) NOT NULL,
+  `saler_contact_number` int(50) NOT NULL,
+  `saler_password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`serial_number`);
+
+--
+-- Indexes for table `saler`
+--
+ALTER TABLE `saler`
+  ADD PRIMARY KEY (`saler_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(250) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `serial_number` int(250) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `saler`
+--
+ALTER TABLE `saler`
+  MODIFY `saler_id` int(250) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
