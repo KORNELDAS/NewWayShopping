f95d8c47598d5ec4357affa52fb7c29749f08619
sachi41999 <sachisabya41999@gmail.com>
2021-06-11 18:31:10 +0530

    commit by sabyasachi

--- databases/newwayshopping.sql
+++ databases/newwayshopping.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3008
--- Generation Time: Jan 20, 2021 at 03:46 PM
+-- Generation Time: Jun 11, 2021 at 02:57 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.0
 
@@ -45,7 +45,9 @@ CREATE TABLE `bidders` (
 
 INSERT INTO `bidders` (`bidder_id`, `name`, `product_name`, `product_id`, `biding_price`, `base_price`, `phone`, `user_email`, `status`) VALUES
 (2, 'rahul kumar', 'Embroidered Fashion Cotton Blend Saree  (Green)', 27, '602', '498', '9973910344', 'rahulkumar41999@gmail.com', NULL),
-(7, 'rahul kumar', 'Woodness Liberty Solid Wood Bunk Bed  (Finish Color - Dark Brown)', 28, '12403', '12371', '9973910344', 'rahulkumar41999@gmail.com', NULL);
+(7, 'rahul kumar', 'Woodness Liberty Solid Wood Bunk Bed  (Finish Color - Dark Brown)', 28, '12403', '12371', '9973910344', 'rahulkumar41999@gmail.com', 'confirm'),
+(8, 'Sabyasachi', 'Woodness Liberty Solid Wood Bunk Bed  (Finish Color - Dark Brown)', 28, '12409', '12371', '6294683141', 'sachisabya41999@gmail.com', NULL),
+(9, 'Sabyasachi', 'Embroidered Fashion Cotton Blend Saree  (Green)', 27, '617', '498', '6294683141', 'sachisabya41999@gmail.com', NULL);
 
 -- --------------------------------------------------------
 
@@ -72,9 +74,10 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_id`, `product_name`, `product_type`, `product_cost`, `product_image`, `product_image_1`, `product_image_2`, `end_date`, `description`, `puser`, `status`) VALUES
-(26, 'Acer Predator Helios 300', 'Laptop', '94990', 'acer laptop1.jpeg', 'acer laptop2.jpeg', 'acer laptop3.jpeg', '2021-01-21T21:54', '\r\nSales Package\r\n2 in 1 Laptop, Power Adaptor, User Guide, Warranty Documents\r\nModel Number\r\n14IIL05\r\nPart Number\r\n81X100NDIN\r\nSeries\r\nIdeapad Flex 5\r\nColor\r\nGraphite Grey\r\nType\r\n2 in 1 Laptop\r\nSuitable For\r\nProcessing & Multitasking\r\nBattery Backup\r\nUpto 10 hours\r\nPower Supply\r\n65 W AC Adapter\r\nMS Office Provided\r\nYes', 'sachisabya41999@gmail.com', 1),
-(27, 'Embroidered Fashion Cotton Blend Saree  (Green)', 'Clothes(Female)', '498', 'girl saree1.jpeg', 'girl saree2.jpeg', 'girl saree3.jpeg', '2021-01-19T22:10', 'Style Code\r\nAARS-001AAAGREEN\r\nPattern\r\nEmbroidered\r\nPack of\r\n1\r\nOccasion\r\nParty & Festive\r\nType of Embroidery\r\nZarodosi Embroidery\r\nHand Embroidery\r\nno\r\nEmbroidery Method\r\nMachine\r\nConstruction Type\r\nmachine\r\nFabric Care\r\nWash with Like Colors\r\nFabric\r\nCotton Blend\r\nType\r\nFashion\r\nBlouse Piece\r\nUnstitched\r\nSari Style\r\nRegular Sari\r\nSari Length\r\n5.5\r\nBlouse Piece Length\r\n0.75\r\nWeight\r\n0.3', 'ghoshsupriya159@gmail.com', 1),
-(28, 'Woodness Liberty Solid Wood Bunk Bed  (Finish Color - Dark Brown)', 'Furniture', '12371', 'furniture1.jpeg', 'furniture2.jpeg', 'furniture3.jpeg', '2021-01-27T22:15', 'Bed Size: Single\r\nBed Design: Standard Bunk Bed\r\nHeight x Width: 1515 mm x 1975 mm (4 ft 11 in x 6 ft 5 in)\r\nKnock Down - Delivered in non-assembled pieces, installation by service partner', 'rachita666@gmail.com', 1);
+(26, 'Acer Predator Helios 300', 'Laptop', '94990', 'acer laptop1.jpeg', 'acer laptop2.jpeg', 'acer laptop3.jpeg', '2021-06-21T21:54', '\r\nSales Package\r\n2 in 1 Laptop, Power Adaptor, User Guide, Warranty Documents\r\nModel Number\r\n14IIL05\r\nPart Number\r\n81X100NDIN\r\nSeries\r\nIdeapad Flex 5\r\nColor\r\nGraphite Grey\r\nType\r\n2 in 1 Laptop\r\nSuitable For\r\nProcessing & Multitasking\r\nBattery Backup\r\nUpto 10 hours\r\nPower Supply\r\n65 W AC Adapter\r\nMS Office Provided\r\nYes', 'sachisabya41999@gmail.com', 1),
+(27, 'Embroidered Fashion Cotton Blend Saree  (Green)', 'Clothes(Female)', '498', 'girl saree1.jpeg', 'girl saree2.jpeg', 'girl saree3.jpeg', '2021-06-11T03:39', 'Style Code\r\nAARS-001AAAGREEN\r\nPattern\r\nEmbroidered\r\nPack of\r\n1\r\nOccasion\r\nParty & Festive\r\nType of Embroidery\r\nZarodosi Embroidery\r\nHand Embroidery\r\nno\r\nEmbroidery Method\r\nMachine\r\nConstruction Type\r\nmachine\r\nFabric Care\r\nWash with Like Colors\r\nFabric\r\nCotton Blend\r\nType\r\nFashion\r\nBlouse Piece\r\nUnstitched\r\nSari Style\r\nRegular Sari\r\nSari Length\r\n5.5\r\nBlouse Piece Length\r\n0.75\r\nWeight\r\n0.3', 'ghoshsupriya159@gmail.com', 1),
+(28, 'Woodness Liberty Solid Wood Bunk Bed  (Finish Color - Dark Brown)', 'Furniture', '12371', 'furniture1.jpeg', 'furniture2.jpeg', 'furniture3.jpeg', '2021-09-27T22:15', 'Bed Size: Single\r\nBed Design: Standard Bunk Bed\r\nHeight x Width: 1515 mm x 1975 mm (4 ft 11 in x 6 ft 5 in)\r\nKnock Down - Delivered in non-assembled pieces, installation by service partner', 'rachita666@gmail.com', 1),
+(29, 'avatar', 'Laptop', '2000', '_DSC0616.JPG', '_DSC0553.JPG', '_DSC0567.JPG', '2021-07-19T04:06', 'fhj,k,', 'sachisabya41999@gmail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -105,12 +108,13 @@ CREATE TABLE `registration` (
 --
 
 INSERT INTO `registration` (`serial_number`, `name`, `email`, `password`, `phone_number`, `alternative_phone_number`, `date_of_birth`, `gender`, `address_1`, `address_2`, `country`, `state`, `postal_code`, `image`, `reg_date`) VALUES
-(55, 'Sabyasachi', 'sachisabya41999@gmail.com', '8bc36b1b8cf0927a05a2268bd8d4d66c', '6294683141', '8809836648', '2020-12-28', 'Male', 'Mahavir Colony, Beur More,Anisabad Patna', 'Mahavir Colony, Beur More,Anisabad Patna\r\nbeur', 'United States', 'Bihar', 800002, 'irrfan-khan-best-hd-photos-download-1080p-whatsapp-dpstatus-images-ommh-667x961.jpg', '2020-12-15 18:28:10'),
+(55, 'Sabyasachi', 'sachisabya41999@gmail.com', '153c69a56beaee5827d08e2f18b14b54', '6294683141', '8809836648', '2020-12-28', 'Male', 'Mahavir Colony, Beur More,Anisabad Patna', 'Mahavir Colony, Beur More,Anisabad Patna\r\nbeur', 'United States', 'Bihar', 800002, 'IMG-20200514-WA0017.jpg', '2020-12-15 18:28:10'),
 (56, 'Gagandeep', 'gagan.training@gmail.com', '526641bd710f0e083d38ed9a216391c3', '7896541230', '7896541230', '1995-10-07', 'Male', 'barabazar', 'chandannagar', 'United States', 'West Bengal', 712136, 'default.png', '2020-12-23 14:04:59'),
 (58, 'supriya ghosh', 'ghoshsupriya159@gmail.com', '8bc36b1b8cf0927a05a2268bd8d4d66c', '7903346686', '8809836648', '2021-01-07', 'Male', 'Mahavir Colony, Beur More,Anisabad Patna', 'Mahavir Colony, Beur More,Anisabad Patna\r\nbeur', 'United States', 'West Bengal', 700012, 'default.png', '2021-01-09 10:05:47'),
 (59, 'shyam kumar', 'shyamkumar444@gmail.com', '8bc36b1b8cf0927a05a2268bd8d4d66c', '8976564783', '9931715679', '2021-01-06', 'Male', 'Chater, P.s Barahara, Bhojpur( Ara)', 'Chater, P.s Barahara, Bhojpur( Ara)', 'United States', 'Bihar', 802163, 'default.png', '2021-01-14 12:17:50'),
 (60, 'rahul kumar', 'rahulkumar41999@gmail.com', '8bc36b1b8cf0927a05a2268bd8d4d66c', '9973910344', '8809836648', '2021-01-23', 'Male', 'Mahavir Colony, Beur More,Anisabad Patna', 'Mahavir Colony, Beur More,Anisabad Patna\r\nbeur', 'United States', 'Bihar', 800002, 'default.png', '2021-01-14 12:19:00'),
-(61, 'rachita jha', 'rachita666@gmail.com', '8bc36b1b8cf0927a05a2268bd8d4d66c', '7903346686', '9931715679', '2021-01-26', 'Female', 'Chater, P.s Barahara, Bhojpur( Ara)', 'Chater, P.s Barahara, Bhojpur( Ara)', 'United States', 'Bihar', 800078, 'default.png', '2021-01-14 12:20:08');
+(61, 'rachita jha', 'rachita666@gmail.com', '8bc36b1b8cf0927a05a2268bd8d4d66c', '7903346686', '9931715679', '2021-01-26', 'Female', 'Chater, P.s Barahara, Bhojpur( Ara)', 'Chater, P.s Barahara, Bhojpur( Ara)', 'United States', 'Bihar', 800078, 'default.png', '2021-01-14 12:20:08'),
+(62, 'Parmila Shah', 'parmilashah419@gmail.com', '153c69a56beaee5827d08e2f18b14b54', '9931790882', '', '1998-02-28', 'Female', 'krishnapur ,arambhag,bradhaman,westbengal-734897', '', 'United States', 'West Bengal', 734897, 'default.png', '2021-05-23 19:22:30');
 
 -- --------------------------------------------------------
 
@@ -130,7 +134,7 @@ CREATE TABLE `reset_link` (
 
 INSERT INTO `reset_link` (`Sl_No`, `Email`, `res_key`) VALUES
 (15, 'gagan.training@gmail.com', '5514b4b4748d01ea4f0d38e4cc385f50'),
-(18, 'sachisabya41999@gmail.com', 'e6126ef30e28b1af14b7983075fcbd9f');
+(18, 'sachisabya41999@gmail.com', 'ecc7dc039500ba33618c6105689ff354');
 
 -- --------------------------------------------------------
 
@@ -217,25 +221,25 @@ ALTER TABLE `saler`
 -- AUTO_INCREMENT for table `bidders`
 --
 ALTER TABLE `bidders`
-  MODIFY `bidder_id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `bidder_id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `product_id` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `registration`
 --
 ALTER TABLE `registration`
-  MODIFY `serial_number` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
+  MODIFY `serial_number` int(250) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT for table `reset_link`
 --
 ALTER TABLE `reset_link`
-  MODIFY `Sl_No` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `Sl_No` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `saler`
